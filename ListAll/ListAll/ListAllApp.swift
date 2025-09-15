e93930023c75d78bc86fc2a1f1eadd7507a52936
Aleksi Sutela <aleksi.sutela@helen.fi>
2025-09-15 18:38:25 +0300

    Init

---
+++ ListAll/ListAll/ListAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAllApp.swift
+//  ListAll
+//
+//  Created by Sutela Aleksi on 15.9.2025.
+//
+
+import SwiftUI
+
+@main
+struct ListAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListAll/ListAllTests/ListAllTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListAllTests.swift
+//  ListAllTests
+//
+//  Created by Sutela Aleksi on 15.9.2025.
+//
+
+import Testing
+@testable import ListAll
+
+struct ListAllTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
